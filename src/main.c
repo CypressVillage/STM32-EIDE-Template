c0d4fc5eb09b3ecdb9674733d1e44e4c77ba4445
cypress <zhaobc3@mail2.sysu.edu.cn>
2024-03-15 22:45:09 +0800

    ✨ TinyMaix for gcc

--- src/main.c
+++ src/main.c
@@ -9,42 +9,6 @@ int main(void)
     module_init();
 
 	while(1){
-		TB6612_Motor_Run(200, 200);
+
 	}
-
-	// while (1)
-	// {
-	// 	// CmdProcess();
-	// 	if(s_cDataUpdate)
-	// 	{
-	// 		// printf("hello world\n");
-	// 		for(i = 0; i < 3; i++)
-	// 		{
-	// 			// fAcc[i] = sReg[AX+i] / 32768.0f * 16.0f;
-	// 			// fGyro[i] = sReg[GX+i] / 32768.0f * 2000.0f;
-	// 			fAngle[i] = sReg[Roll+i] / 32768.0f * 180.0f;
-	// 		}
-	// 		// if(s_cDataUpdate & ACC_UPDATE)
-	// 		// {
-	// 		// 	printf("acc:%.3f,%.3f,%.3f\n", fAcc[0], fAcc[1], fAcc[2]);
-	// 		// 	s_cDataUpdate &= ~ACC_UPDATE;
-	// 		// }
-	// 		// if(s_cDataUpdate & GYRO_UPDATE)
-	// 		// {
-	// 		// 	printf("gyro:%.3f,%.3f,%.3f\n", fGyro[0], fGyro[1], fGyro[2]);
-	// 		// 	s_cDataUpdate &= ~GYRO_UPDATE;
-	// 		// }
-	// 		if(s_cDataUpdate & ANGLE_UPDATE)
-	// 		{
-	// 			printf("angle:%.3f,%.3f,%.3f\n", fAngle[0], fAngle[1], fAngle[2]);
-	// 			s_cDataUpdate &= ~ANGLE_UPDATE;
-	// 		}
-	// 		// if(s_cDataUpdate & MAG_UPDATE)
-	// 		// {
-	// 		// 	printf("mag:%d,%d,%d\n", sReg[HX], sReg[HY], sReg[HZ]);
-	// 		// 	s_cDataUpdate &= ~MAG_UPDATE;
-	// 		// }
-	// 	}
-	// }
-
 }
